4c3d3cef9ce08484824c44700adbb18350925630
romablog <iromablog@gmail.com>
2016-02-18 10:12:38 +0300

    Registry modified

--- routes/registry.js
+++ routes/registry.js
@@ -9,17 +9,26 @@ exports.post = function(req, res, next) {
     var about = req.body.about;
     var theme = req.body.theme;
     var language = req.body.language;
-    Model.User.create({
-        authId: uuid.v1(),
-        theme: theme,
-        language: language,
-        firstName: username,
-        lastName: surname,
-        email: email,
-        about: about,
-        password: password
-    }). then(function(user){
-        req.session.user = user.authId;
-        res.send(user)
+    Model.User.findOne({where: {email: email}}).then(function(user){
+        if (user)
+            return null;
+        return Model.User.create({
+            authId: uuid.v1(),
+            theme: theme,
+            language: language,
+            firstName: username,
+            lastName: surname,
+            email: email,
+            about: about,
+            password: password
+        })
+    }).then(function(user){
+        if (user){
+            req.session.user = user.authId;
+            res.send(user)
+        } else {
+            req.session.user = null;
+            res.sendStatus(403);
+        }
     });
 };
